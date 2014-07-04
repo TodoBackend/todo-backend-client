37f2c66d0bd545844afecf3cd566a11aed94ee00
Pete Hodgson <git@thepete.net>
2014-07-04 10:14:53 -0700

    remove BB.localStorage, add some info to the UI, point to a heroku-based backend

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -7,16 +7,12 @@ var app = app || {};
 	// Todo Collection
 	// ---------------
 
-	// The collection of todos is backed by *localStorage* instead of a remote
-	// server.
 	var Todos = Backbone.Collection.extend({
 		// Reference to this collection's model.
 		model: app.Todo,
 
-    url: "http://localhost:9292/todos",
-
-		// Save all of the todo items under the `"todos"` namespace.
-		//localStorage: new Backbone.LocalStorage('todos-backbone'),
+    // FIXME: this should be configurable
+    url: "http://todo-backend-sinatra.herokuapp.com/todos",
 
 		// Filter down the list of all todo items that are finished.
 		completed: function () {
